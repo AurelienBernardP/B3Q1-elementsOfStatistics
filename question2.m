cfb0efeaded837063c12fa45c377422540842559
aurelienbernard <aurelienpoolen@gmail.com>
2019-11-26 12:33:00 +0100

    working question2

--- question2.m
+++ question2.m
@@ -1,14 +1,20 @@
-function createPop
-    pop = population(readtable(’data.csv’), 13);
-    writetable(pop, ’population.csv’);
+function question2
+    pop = createPop;
+    answerQ2a(pop)
+    answerQ2b(pop)
+end
+
+function pop = createPop
+    pop = population(readtable('data.csv'), 13);
+    writetable(pop, 'population.csv');
 end
 
 function answerQ2a(pop)
     sample = iid_sample(pop, 50);
-    ai = sample_sum_up(sample.charges)
-    aii = boxplot(sample.charges)
-    cdfplot(sample.charges)
-    aiii = ks_distance(sample.charges, pop.charges)
+    disp('iid sum up' + sample_sum_up(sample.Charges));
+    boxplot(sample.Charges)
+    cdfplot(sample.Charges)
+    disp('ks distance between iid sample and total population' + ks_distance(sample.Charges, pop.Charges))
 end
 
 function answerQ2b(pop)
@@ -17,8 +23,15 @@ function answerQ2b(pop)
 
     for i = 1:500
         t = iid_sample(pop, 50);
-        A[1,2,3][i] = sample_sum_up(t.charges);
-        A[4][i] = ks_distance(t.charges, pop.charges);
+       [ A(1,i), A(2 ,i), A(3 ,i)] = sample_sum_up(t.Charges);
+        A(4 ,i) = ks_distance(t.Charges, pop.Charges);
     end
-
+    
+    bi = histogram(A(1,:))
+    bii = histogram(A(2,:))
+    biii = histogram(A(3,:))
+    biv = histogram(A(4,:))
 end
+
+
+        
