a0cc11287cea53010c4f333b481518fa0a560d39
aurelienbernard <aurelienpoolen@gmail.com>
2019-11-29 17:01:05 +0100

    moyennes des idds added et aussi titres aux graphe

--- question2.m
+++ question2.m
@@ -6,8 +6,7 @@ function question2()
 end
 
 function pop = createPop()
-    pop = population(readtable('data.csv'), 13);
-    writetable(pop, 'population.csv');
+    pop = readtable('population.csv');
 end
 
 function answerQ2a(pop)
@@ -31,14 +30,32 @@ function answerQ2b(pop)
        [ A(1,i), A(2 ,i), A(3 ,i)] = sample_sum_up(t.Charges);
         A(4 ,i) = ks_distance(t.Charges, pop.Charges);
     end
+    moyeneesDesMoyennes = mean(A(1, :))
+    moyeneesDesMedianes = mean(A(2, :))
+    moyeneesDesSTD = mean(A(3, :))
+    moyeneesDesKS = mean(A(4, :))
+    
     figure()
     bi = histogram(A(1,:));
+    title('Moyennes des frais hospitaliers de 500 �chatillons iid de taille 50')
+    xlabel('Moyennes') 
+    ylabel('Occurences') 
+    
     figure()
     bii = histogram(A(2,:));
+    title('M�dianes des frais hospitaliers de 500 �chatillons iid de taille 50')
+    xlabel('M�dianes') 
+    ylabel('Occurences') 
     figure()
     biii = histogram(A(3,:));
+    title('�carts-type des frais hospitaliers de 500 �chatillons iid de taille 50')
+    xlabel('�carts-type') 
+    ylabel('Occurences') 
     figure()
     biv = histogram(A(4,:));
+    title('Distances KS des frais hospitaliers de 500 �chatillons iid\n de taille 50 par rapport � la populatiion totale')
+    xlabel('Distances de Kolmogorov Smirnov') 
+    ylabel('Occurences') 
 end
 
 
