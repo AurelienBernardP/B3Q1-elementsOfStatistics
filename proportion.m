319d0334fd8047ea0e14ff66f2b0326c652c1d8d
Safadi <tasnim.safadi@student.uliege.be>
2019-12-04 18:47:26 +0100

    Replace proportion.m

--- proportion.m
+++ proportion.m
@@ -1,11 +1,12 @@
 function prop = proportion(x, y)
 %   PROPORTION(X, Y) gives the proportion of elements in X that are smaller
-%   or equal than Y.
+%   or equal than Y and greater than the charges of Smith.
 %
 %   X : N-by-1 double
 %   Y : 1-by-1 double
 %   PROP : 1-by-1 double
 
-
+	smithCharges = groupData(x.LastName == 'Smith', 7);
+	prop = (sum(x<=y & x> smithCharges))/size(x, 1);
     
 end
