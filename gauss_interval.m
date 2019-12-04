5f79ee609cefde9b99a64f560d286968d574ff36
Safadi <tasnim.safadi@student.uliege.be>
2019-12-04 18:46:31 +0100

    Replace gauss_interval.m

--- gauss_interval.m
+++ gauss_interval.m
@@ -1,32 +1,18 @@
-function [cptGauss] = gauss_interval(x)
-%function [lower_bound, upper_bound] = gauss_interval(x, perc)
+function [lower_bound, upper_bound] = gauss_interval(x, sigma, perc)
 %   GAUSS_INTERVAL(X, PERC) computes the lower and upper bounds of the 100*PERC percent Gauss confidence interval
-%   of X's distribution mean.
+%   of X s distribution mean. SIGMA is the standard deviation of the population.
 %
 %   X : N-by-1 double
+%   SIGMA : 1-by-1 double
 %   PERC : 1-by-1 double in [0, 1]
 %   LOWER_BOUND : 1-by-1 double
 %   UPPER_BOUND : 1-by-1 double
 
-    meanX = zeros(100:1);
-    for i=1:100
-        meanX(i, 1) = mean(iid_sample(x, 20));
-    end
-    cptGauss = 0;
-    sigma = std(x);
-    meanBMI = mean(x);
+    meanX = mean(x);
+    u = norminv(1-(1-perc)/2);
+    sizeSqrt = sqrt(size(x,1));
 
-    lower_bound = zeros(1,100);
-    upper_bound = zeros(1,100);
-    
-    %refaire formule pour perc
-    for i=1:100
-        lower_bound(i) = meanX(i) - norminv(0.975) * (sigma/sqrt(20));
-        upper_bound(i) = meanX(i) + norminv(0.975) * (sigma/sqrt(20));
-        
-        if meanBMI >= lower_bound(i) && meanBMI <= upper_bound(i)
-            cptGauss = cptGauss + 1;
-        end
-    end
+    lower_bound = meanX - u * (sigma/sizeSqrt);
+    upper_bound = meanX + u * (sigma/sizeSqrt);
 
 end
