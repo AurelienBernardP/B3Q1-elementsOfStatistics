bf99b00b35d56decf7cbb2347fd6375c4cd80470
Safadi <tasnim.safadi@student.uliege.be>
2019-12-04 18:44:27 +0100

    Replace student_interval.m

--- student_interval.m
+++ student_interval.m
@@ -1,31 +1,18 @@
-function [cptStudent] = student_interval(x)
-%function [lower_bound, upper_bound] = student_interval(x, perc)
+function [lower_bound, upper_bound] = student_interval(x, perc)
 %   STUDENT_INTERVAL(X, PERC) computes the lower and upper bounds of the 100*PERC percent Student confidence interval
-%   of X's distribution mean.
+%   of X s distribution mean.
 %
 %   X : N-by-1 double
 %   PERC : 1-by-1 double in [0, 1]
 %   LOWER_BOUND : 1-by-1 double
 %   UPPER_BOUND : 1-by-1 double
 
-    meanX = zeros(100:1);
-    for i=1:100
-        meanX(i, 1) = mean(iid_sample(x, 20));
-    end
-    cptStudent = 0;
-    meanBMI = mean(x);
-
-    lower_bound = zeros(1,100);
-    upper_bound = zeros(1,100);
+	sizeX = size(x, 1);
+	meanX = mean(x);
+	stdX = std(x);
+    t = tinv(1-((1-perc)/2), size-1);
     
-    %refaire formule pour perc
-    for i=1:100
-        lower_bound(i) = meanX(i,1) - 2.093 * (std(iid_sample(x, 20))/sqrt(20));
-        upper_bound(i) = meanX(i,1) + 2.093 * (std(iid_sample(x, 20))/sqrt(20));
-        
-        if meanBMI >= lower_bound(i) && meanBMI <= upper_bound(i)
-            cptStudent = cptStudent + 1;
-        end
-    end
+    lower_bound = meanX - t * (stdX/sqrt(sizeX));
+    upper_bound = meanX + t * (stdX/sqrt(sizeX));
     
 end
