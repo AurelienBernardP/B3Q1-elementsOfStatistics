dedf94b584e479789b60b4bd86ccb5f373ed0d11
Safadi <tasnim.safadi@student.uliege.be>
2019-12-05 21:33:51 +0100

    Replace question1.m

--- question1.m
+++ question1.m
@@ -1,50 +1,66 @@
 
 
-function answerQ1(pop)
-
+function question1()
+    pop = readtable('test.csv');
+    chargesPop = pop.Charges;
+    
     figure()
-    histogram(pop.Charges);
+    histogram(chargesPop, 20);
     title('Frais hospitaliers de la population')
-    xlabel('Frais hospitaliers')
+    xlabel('Frais hospitaliers en dollar')
     ylabel('Nombre de personnes')
 
     figure()
-    boxplot(pop.Charges);
+    boxplot(chargesPop);
     title('Frais hospitaliers de la population');
-    ylabel('Frais hospitaliers')
+    ylabel('Frais hospitaliers en dollar');
 
     figure()
-    cdfplot(pop.Charges);
+    cdfplot(chargesPop);
     title('Polygone des frequences cumulees des frais hospitaliers');
-    xlabel('Frais hospitaliers');
+    xlabel('Frais hospitaliers en dollar');
     ylabel('Frequence cumulee');
 
-    male = groupData(pop.Sex == 0, :);
+    male = pop(pop.Sex == 0, :);
     figure()
-    scatter(male.Charges,male.Ages);
+    scatter(male.Charges,male.Age);
     title('Frais hospitaliers des hommes selon leurs ages')
-    xlabel('Frais hospitaliers')
+    xlabel('Frais hospitaliers en dollar')
     ylabel('Ages')
-
-    female = groupData(pop.Sex == 1, :);
+    corr_m = correlation(male.Charges,male.Age)
+    
+    female = pop(pop.Sex == 1, :);
     figure()
-    scatter(female.Charges,female.Ages);
+    scatter(female.Charges,female.Age);
     title('Frais hospitaliers des femmes selon leurs ages')
-    xlabel('Frais hospitaliers')
+    xlabel('Frais hospitaliers en dollar')
     ylabel('Ages')
+    corr_f = correlation(female.Charges,female.Age)
 
-    male = groupData(pop.Sex == 0 & pop.Charges<18786, :);
+    male = pop(pop.Sex == 0 & pop.Charges<18786, :);
     figure()
-    scatter(male.Charges,male.Ages);
+    scatter(male.Charges,male.Age);
     title('Frais hospitaliers des hommes selon leurs ages')
-    xlabel('Frais hospitaliers')
+    xlabel('Frais hospitaliers en dollar')
     ylabel('Ages')
+    corr_nm = correlation(male.Charges,male.Age)
 
-    female = groupData(pop.Sex == 1 & pop.Charges<18786, :);
+    female = pop(pop.Sex == 1 & pop.Charges<18786, :);
     figure()
-    scatter(female.Charges,female.Ages);
+    scatter(female.Charges,female.Age);
     title('Frais hospitaliers des femmes selon leurs ages')
-    xlabel('Frais hospitaliers')
+    xlabel('Frais hospitaliers en dollar')
     ylabel('Ages')
+    corr_nf = correlation(female.Charges,female.Age)
 
+    smithCharges = pop(strcmp(pop.LastName, 'Smith'),:).Charges;
+    [mean_x, median_x, std_x] = sum_up(chargesPop)
+    [lower_inter, upper_inter] = normal_interval(chargesPop)
+    prop_1b = proportion(chargesPop, smithCharges)
+    prop_1c = proportion(chargesPop, upper_inter) - proportion(chargesPop, lower_inter)
+    [first_q, second_q, third_q] = quartiles(chargesPop)
+    prop_1d =  sum(chargesPop > third_q + (0.5 * (third_q - first_q)))
+    prop_1e = proportion(chargesPop, 25000) - proportion(chargesPop, smithCharges)
+    
+    
 end
