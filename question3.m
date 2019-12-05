e3e19183fd2e7d0f37b1c720564f0cc0dd7e037a
Safadi <tasnim.safadi@student.uliege.be>
2019-12-05 21:34:27 +0100

    Replace question3.m

--- question3.m
+++ question3.m
@@ -1,6 +1,8 @@
 
-function [meanBias,meanVariance, medianBias, medianVariance] = answerQ3(x, size)
+function question3(size)
     
+    pop = readtable('test.csv');
+    x = pop.BMI;
     meanX = zeros(100:1);
     medianX = zeros(100:1);
     for i=1:100
@@ -8,9 +10,15 @@ function [meanBias,meanVariance, medianBias, medianVariance] = answerQ3(x, size)
         medianX(i, 1) = median(iid_sample(x, size));
     end
     
-    meanBias = mean(meanX(:,1)) - mean(x);
-    meanVariance = var(meanX(:,1));
-    medianBias = mean(medianX(:,1)) - median(x);
-    medianVariance = var(medianX(:,1));
-
+    meanBias = mean(meanX(:,1)) - mean(x)
+    meanVariance = var(meanX(:,1))
+    medianBias = mean(medianX(:,1)) - median(x)
+    medianVariance = var(medianX(:,1))
+    
+    figure()
+    histogram(x, 20);
+    title('BMI de la population')
+    xlabel('BMI en kg/m^2')
+    ylabel('Nombre de personnes')
+    
 end
